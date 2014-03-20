1fce4405c6a25c15fa0375f53b83fc77c9866082
Emma Lou <louemma2@gmail.com>
2014-03-20 01:43:23 -0400

    Don't know how to do toString

--- 08-linkedLists/MyLinkedList.java
+++ 08-linkedLists/MyLinkedList.java
@@ -22,8 +22,85 @@ public class MyLinkedList {
 	}
 	return s;
     }
+
     public void add (int i, String d){
 	Node tmp = new Node (d);
+	for(int a = 0; a <= i; i++){
+	    Node tmptmp = head.getNext();
+	    head = tmptmp;
+	    if (a == i-1){
+		tmptmp.setNext(head.getNext());
+		head.setNext(tmptmp);
+	    }
+	    if (tmptmp.getNext() == null){
+		System.out.println("You're outta bounds");
+	    }
+	}
+    }
+
+    public String get(int i){
+	String ret = "";
+	for (int a = 0; a < i; i++){
+	    Node tmp = head.getNext();
+	    head = tmp;
+	    if (a == i-1){
+		ret = head.getData();
+	    }
+	    if (tmp.getNext() == null){
+		ret = "No good";
+	    }
+	}
+	return ret;
+    }
+
+    public String set(int i, String s){
+	String ret = "";
+        for (int a = 0; a < i; i++){
+	    Node tmp = head.getNext();
+	    head = tmp;
+	    if (a == i-1){
+		ret = head.getData();
+		head.setData(s);
+	    }
+	}
+	return ret;
+    }
+
+    public String remove(int i){
+	String ret = "";
+	for (int a = 0; a < i; i++){
+	    Node tmp = head.getNext();
+	    head = tmp;
+	    if (a == i-1){
+		ret = head.getNext().getData();
+		Node tmptmp = head.getNext().getNext();
+		head.setNext(tmptmp);
+	    }
+	}
+	return ret;
+    }
+
+    public int find(String s){
+	int count = 0;
+	int fin = 0;
+	while (head.getNext() != null){
+	    Node tmp = head.getNext();
+	    head = tmp;
+	    if (head.getData() == s){
+		fin = count;
+	    }
+	    count++;
+	}
+	return fin;
+    }
+    public int length(){
+	int count = 0;
+	while (head.getNext() != null){
+	    Node tmp = head.getNext();
+	    head = tmp;
+	    count++; 
+	}
+	return count + 1;
     }
 
 }
