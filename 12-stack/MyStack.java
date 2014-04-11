4b4b8a6018313f310bd6347b3499f1dcf69d45f2
Emma Lou <louemma2@gmail.com>
2014-04-11 08:43:47 -0400

    Updated

--- 12-stack/MyStack.java
+++ 12-stack/MyStack.java
@@ -5,51 +5,40 @@ public class MyStack{
 
     private int top;
     private String[]stack;
-    private int numelt;
-    private int len;
-
+    
     public MyStack(){
 	top = 0;
 	stack = new String[10];
-	numelt = 0;
-	len = stack.length;
     }
 
     public void push(String s){
-	String[]tmp;
-        String str = new String(s);
-	if (numelt >=10){
-	    tmp = new String[len + 1];
-	}
-	else{
-	    tmp = new String[len];
-	}
-	tmp[top] = str;
-	for(int i = 0; i < len-1; i++){
-	    tmp[i+1] = stack[i];
-	}
-	numelt = numelt + 1;
-	stack = tmp;
+	if (top<stack.length){
+            stack[top] = s;
+            top++;
+        }
+        else{
+	    stack = Arrays.copyOf(stack,stack.length * 2);
+	    push(s);
+        }
     }
 
     public String pop(){
-	String ret = stack[0];
-	for (int i = 0; i < len-1; i++){
-	    stack[i] = stack[i+1];
-	}
-	numelt = numelt - 1;
-	return ret;
+	String tmp = stack[top - 1 ];
+	stack[top - 1] = null;
+	top--;
+	return tmp;
     }
 
     public String peek(){
-	return stack[0];
+	return stack[top-1];
     }
 
     public String toString(){
 	String s = "";
-	for (int i = 0; i < len; i++){
+	for (int i = 0; i < top; i++){
 	    s = s + stack[i] + ", ";
 	}
 	return s;
     }
 }
+
